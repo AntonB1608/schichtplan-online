bac46efef56c75ac6b987713c6300d9c1ec28691
Anton Boewer <antonboewer@icloud.com>
2026-05-25 19:47:41 +0200

    feat: add POST route to receive form data

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return 
+    return render_template("/index.html")
+
+@app.route("/schicht", methods=["POST"])
+datum = request.form["datum"]
+zeit = request.form["zeit"]
+def schicht_eintragen():
+    index()
 
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', port = 5555, debug=True)
