00dc4dd80615c2fbc6bf5baf168b47ea7db8a579
Anton Boewer <antonboewer@icloud.com>
2026-05-28 19:45:15 +0200

    fix: remove leading slash from render_template path

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request
 from datetime import datetime
-
+ 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("/index.html")
+    return render_template("index.html")
 
 @app.route("/schicht", methods=["POST"])
 def schicht_eintragen():
@@ -14,8 +14,15 @@ def schicht_eintragen():
     zeit_anfang = request.form["zeit_anfang"]
     zeit_ende = request.form["zeit_ende"]
     datum_formatiert = datetime.strptime(datum, "%Y-%m-%d").strftime("%d.%m.%Y")
-    with open(pfad, "a", encoding="utf-8") as f:
-        f.write(f"{datum_formatiert} {zeit_anfang}-{zeit_ende}\n")
+    frei = request.form.get("frei")
+    if frei:
+        with open(pfad, "a", encoding="utf-8") as f:
+            f.write(f"{datum_formatiert} frei\n")
+    else:
+        with open(pfad, "a", encoding="utf-8") as f:
+            f.write(f"{datum_formatiert} {zeit_anfang}-{zeit_ende}\n")
+
+    
     return "Schicht eingetragen!"
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', port = 5555, debug=True)
